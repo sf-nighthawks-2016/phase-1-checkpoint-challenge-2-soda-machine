c282fb56916a0b9efd4fc094fd5b7653ea43b2b5
Marshall Agharanya <email": null>
2016-05-31 09:58:34 -0700

    Pair marshyup Challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,4 @@
+require_relative 'soda'
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +8,24 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    sodas.find { |soda|
+    soda.brand == soda_brand}
+
   end
 
   def sell(soda_brand)
+    if soda_brand == find_soda(soda_brand)
+    sodas.delete(soda_brand)
+    @cash += soda.price
+    end
   end
-
 end
+
+#Runner
+# s1 = Soda.new(brand: 'pepsi', price: 0.65)
+# s2 =  SodaMachine.new(sodas: ['pepsi', 'mountain_dew'], cash: 1.00)
+# p s2.find_soda('pepsi')
