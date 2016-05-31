2cecbc274d0ca6d64125b317eebd203186da6351
Spencer Dezart-Smith <email": null>
2016-05-31 09:58:43 -0700

    Checkpoint challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,27 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.length
   end
 
   def find_soda(soda_brand)
+    if sodas.brand.include?(soda_brand)
+      index = sodas.find_index(soda_brand)
+      soda = sodas.fetch(index)
+      return soda
+    else
+      return nil
+    end
   end
 
   def sell(soda_brand)
-  end
+    if sodas.include?(soda_brand) == false
+      return nil
+    end
 
+  end
 end
+
+# Biggest struggle is reading and interpreting the rpec tests.
+# If i was to build the class it would flow clearly but working backwards struggle
+
