aabf8ad2f7fb94f09639da34b5fccbf87886cf2e
Kenneth Olson <email": null>
2016-05-31 09:55:59 -0700

    got nothing to pass, but put some cod ein to show thought process

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,29 @@
+require_relative 'soda'
+
 class SodaMachine
-  attr_reader :sodas, :cash
+  attr_reader :sodas, :cash, :inventory
 
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    @inventory = []
   end
 
   def current_inventory_count
+    @inventory << Soda.new
   end
 
   def find_soda(soda_brand)
+    if @inventory.include?(soda_brand)
+      true
+    return soda_brand
+    end
   end
 
   def sell(soda_brand)
   end
 
 end
+
+p Soda.new(brand: 'Pepsi', price: 0.65)
+p current_inventory_count
