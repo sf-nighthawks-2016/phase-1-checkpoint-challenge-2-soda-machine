369982167ffc3cf8d9a71a99c8efa96a1de3d9b6
Samuel Heinz <samueljheinz@gmail.com>
2016-05-31 10:00:59 -0700

    Soda machine works(ish)

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,10 +7,41 @@ class SodaMachine
   end
 
   def current_inventory_count
+    arr = []
+    @sodas.each do |key, value|
+      arr << key
+    end
+    if arr.length > 0
+      return arr.length
+    else 
+      return 0
+    end
   end
 
   def find_soda(soda_brand)
+
+    if current_inventory_count > 0
+       if soda_brand == "Pepsi"
+         return @sodas[0]
+         @sodas = @sodas - [@sodas[0]]
+       elsif soda_brand == "Mountain Dew"
+        return @sodas[1]
+         @sodas = @sodas - [@sodas[1]]
+       elsif soda_brand == "Coke Zero"
+        return @sodas[2]
+         @sodas = @sodas - [@sodas[2]]
+       elsif soda_brand == "Mountain Dew"
+        return @sodas[1]
+         @sodas = @sodas - [@sodas[1]]
+       else
+        return nil
+       end
+    else 
+     return "no sodas left"
+    end
   end
+
+ 
 
   def sell(soda_brand)
   end
