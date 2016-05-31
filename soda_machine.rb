ade87f3b21560b5e5fdda441b9996431f47a9336
Erica Yingling <eyingling02@gmail.com>
2016-05-31 10:01:55 -0700

    not finished debugging #find_soda, #sell, completed current_inventory_count

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,33 @@
 class SodaMachine
-  attr_reader :sodas, :cash
+  require_relative "soda"
+  attr_reader :sodas, :cash, :price, :inventory
 
   def initialize(args = {})
     @sodas = args[:sodas]
+    @price = args[:price]
     @cash = args[:cash]
+    # @cash = :price.reduce(:+) 
   end
 
   def current_inventory_count
+    p soda_count = @sodas.count
+    return soda_count
   end
 
   def find_soda(soda_brand)
+    @sodas.map do |word|
+      p word.split("_")
+    end
+    @sodas.include?(soda_brand) ? soda_brand : nil
   end
 
   def sell(soda_brand)
+    # @inventory.delete!(soda_brand)
+    # return @inventory
   end
 
 end
+
+
+erica = SodaMachine.new(sodas: ["pepsi", 'mountain_dew', 'coke_zero', 'second_pepsi'], cash: 1.00) 
+p erica.find_soda("pepsi")
