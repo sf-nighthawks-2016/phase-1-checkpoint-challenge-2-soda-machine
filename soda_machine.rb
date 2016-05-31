627b897851ed9b0bf1d0a677af91ef9a6226fe49
Aaron Kim <aaronkim1205@gmail.com>
2016-05-31 09:51:39 -0700

    Trying to get this to work

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,25 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |soda, price|
+      if @soda == soda_brand
+        return soda_brand
+      else
+        return nil
+      end
   end
 
   def sell(soda_brand)
+    if soda_brand <= 0
+      return true
+    else
+      return soda_brand
+    end
   end
 
+end
 end
