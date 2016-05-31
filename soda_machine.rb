113c5c5d748fc50a2e849ea6fbc59ac765915342
maxwellworkman <email": null>
2016-05-31 09:49:41 -0700

    add soda_machine methods

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,19 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.length
   end
 
   def find_soda(soda_brand)
+    sodas.find { |soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    index = sodas.find_index { |soda| soda.brand == soda_brand}
+    if index != nil
+      @cash += sodas[index].price
+      sodas.delete_at(index)
+    end
   end
 
 end
