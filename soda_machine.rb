519a2f4753e8f7df541cd4389da0164f67d9c5fb
Sebastian Escobedo <email": null>
2016-05-31 10:02:04 -0700

    Add methods to pass test in rspec.

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,4 @@
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +8,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    #this needs to return
+    #the number of sodas that it contains
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    #Be able to find single soda
+    #with a given brand
+    @sodas.find { |soda| soda.brand == soda_brand}
+
   end
 
   def sell(soda_brand)
+    #When soda is sold it should be removed from soda
+    #machine and the price of the soda should be added
+    #to machine cash.
+    return nil if @sodas.empty?
+    soda = find_soda(soda_brand)
+    @soda.delete
+
   end
 
 end
