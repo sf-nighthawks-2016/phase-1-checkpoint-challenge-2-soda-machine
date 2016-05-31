c9ec030011de8f978d9975d9caa86565f2e07d7e
Michael Furlong <email": null>
2016-05-31 10:00:31 -0700

    add sell method

--- soda_machine.rb
+++ soda_machine.rb
@@ -6,6 +6,7 @@ class SodaMachine
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+   # @new_soda = Soda.new
   end
 
   # RETURNS current inventory count
@@ -15,16 +16,29 @@ class SodaMachine
 
   # finds a certain soda in the machine
   def find_soda(soda_brand)
-    @sodas.each  do |soda|
-      if soda == soda_brand
-        return soda_brand
-      else
-        nil
-      end
+
+    sodas.find do |soda|
+      soda.brand == soda_brand
     end
+
+   # @sodas.each do |soda|
+   #     return @soda
+   #   else
+   #    return nil
+   #   end
+   # end
   end
 
   def sell(soda_brand)
+    i = @sodas.find_index do |soda|
+      soda.brand == soda_brand}
+    end
+
+    if i != nil
+
+    @cash += sodas[i].price
+    sodas.delete_at(i)
+  end
   end
 
 end
