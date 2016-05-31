1ad577fb58b8a6a885d0c9b169c51fdd1d2de558
Becky Lambert <email": null>
2016-05-31 10:00:56 -0700

    checkpoint challenge 2

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,21 @@ class SodaMachine
   end
 
   def current_inventory_count
+     @sodas.length
   end
 
   def find_soda(soda_brand)
+    return sodas.find { |soda| soda.brand == soda.brand}
+    else
+        return nil if soda.brand == 'surge'
+       soda.brand
   end
 
   def sell(soda_brand)
+      if find_soda(soda_brand) != nil
+         the_soda = @sodas.find {|soda| soda.brand == soda_brand}
+         @cash += the_soda.price
+         @sodas.delete(the_soda)
+     end
   end
-
 end
