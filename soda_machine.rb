b88340903cf952e0fa8726b58a7f8ddd04993ed7
nick bushell <email": null>
2016-05-31 09:58:35 -0700

    checkpoint

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,4 @@
+require_relative 'Soda'
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +8,18 @@ class SodaMachine
   end
 
   def current_inventory_count
+  args[:sodas].length
   end
 
   def find_soda(soda_brand)
+    args[:sodas].include?(soda_brand)
+    return soda_brand
   end
 
   def sell(soda_brand)
+    args.delete(soda_brand)
+    args[:cash] = args[:cash] + args[:price]
+
   end
 
 end
