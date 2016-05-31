eb37a7f7d9ab356e2c6b92415d060a67fccc7e15
Michael Furlong <email": null>
2016-05-31 09:28:21 -0700

    add soda details add find soda method

--- soda.rb
+++ soda.rb
@@ -1,6 +1,8 @@
 class Soda
   attr_reader :brand, :price
 
+  # we'll be taking an arguments as a HASH!
+
   def initialize(args = {})
     @brand = args[:brand]
     @price = args[:price]
--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -6,10 +8,20 @@ class SodaMachine
     @cash = args[:cash]
   end
 
+  # RETURNS current inventory count
   def current_inventory_count
+    return @sodas.length
   end
 
+  # finds a certain soda in the machine
   def find_soda(soda_brand)
+    @sodas.each  do |soda|
+      if soda == soda_brand
+        return soda_brand
+      else
+        nil
+      end
+    end
   end
 
   def sell(soda_brand)
