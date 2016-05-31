ee313d98958ebaf8052c529654763ec9e35b1fd7
Mikey Moore <email": null>
2016-05-31 09:49:20 -0700

    iterate through soda brands to return a soda if find_soda is true

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative "soda"
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -11,9 +13,21 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        return soda
+      else
+        return nil
+      end
+  end
   end
 
   def sell(soda_brand)
   end
 
 end
+
+# drink = Soda.new(brand: 'Pepsi', price: 0.65)
+# machine = SodaMachine.new(sodas: [drink], cash: 1.00)
+# drink.sodas
+
