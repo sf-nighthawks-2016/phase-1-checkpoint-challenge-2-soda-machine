c89c3cea163f5558011e184de11ecbb63938b114
bootcoder <bootcoder@gmail.com>
2016-06-03 15:07:22 -0700

    update notes

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,10 +1,32 @@
+# class Soda
+#   # attr_accessor :brand
+
+#   def initialize(args = {})
+#     @brand = "Coca-cola"
+#   end
+
+#   def do_stuff
+#     # p @brand
+#     @brand = "Pepsi"
+#     @thing = Thing.new(thing_data: @brand)
+#   end
+
+
+# end
+
 class SodaMachine
   attr_reader :sodas, :cash
 
   def initialize(args = {})
-    @sodas = args[:sodas]
+    # @sodas = []
+    @sodas = args.fetch(:sodas, [])
     @cash = args[:cash]
+    # @sodas = stock_machine
   end
+
+  # def stock_machine
+  #   Parser.import_sodas
+  # end
 
   def current_inventory_count
     sodas.length
@@ -15,10 +37,17 @@ class SodaMachine
   end
 
   def sell(soda_brand)
-    sold = @sodas.find { |soda| soda.brand == soda_brand }
-    return nil if sold == nil
+    # sold = @sodas.find { |soda| soda.brand == soda_brand }
+    sold = find_soda(soda_brand)
+    # return nil if sold == nil
+    return nil if sold.nil?
     @cash += sold.price
     @sodas.delete(sold)
   end
 
 end
+
+ # p sada = SodaMachine.new(sodas: [Soda.new()], cash: 1.0)
+
+ # p " * " * 30
+ # p sada.sodas.first.brand = "Dr Pepper"
