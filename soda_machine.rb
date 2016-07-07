cead01f66c3bdc742c8bcbbb36d5d889f2d9776e
Mikael <miketekle14@gmail.com>
2016-07-07 16:26:26 -0700

    finish light weight brain teaser

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,22 @@ class SodaMachine
   end
 
   def current_inventory_count
+    self.sodas.count
   end
 
   def find_soda(soda_brand)
+    self.sodas.find {|soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    return nil unless @sodas.find {|soda| soda.brand == soda_brand}
+    
+    @sodas.find do |soda|
+      if soda.brand == soda_brand
+        @cash += soda.price
+        @sodas.delete(soda)
+      end
+    end
   end
 
 end
